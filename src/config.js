d8563029d5b0dfe1340cf330963868b38133b7f1
Bilbertius <lauchlinmacneil@gmail.com>
2020-02-27 13:10:25 -0700

    fix config

--- src/config.js
+++ src/config.js
@@ -2,6 +2,7 @@ module.exports = {
 	PORT: process.env.PORT || 8000,
 	NODE_ENV: process.env.NODE_ENV || 'development',
 	DATABASE_URL:process.env.DATABASE_URL || '$(heroku config:get DATABASE_URL -a your-app)',
-	JWT_SECRET: process.env.JWT_SECRET,
-	CLIENT_ORIGIN: process.env.CLIENT_ORIGIN || 'https://jamclouds.now.sh/'
+	JWT_SECRET: process.env.JWT_SECRET || 'dummy-jwt-secret',
+	CLIENT_ORIGIN: process.env.CLIENT_ORIGIN || 'https://jamclouds.now.sh/',
+	API_KEY: process.env.API_KEY,
 };
