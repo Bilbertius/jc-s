75a0ca9bf9df3da52b3922c7fce64f51bfcb5f9e
Bilbertius <lauchlinmacneil@gmail.com>
2020-02-26 23:29:16 -0700

    fudge

--- src/config.js
+++ src/config.js
@@ -3,5 +3,5 @@ module.exports = {
 	NODE_ENV: process.env.NODE_ENV || 'development',
 	DATABASE_URL:process.env.DATABASE_URL || '$(heroku config:get DATABASE_URL -a your-app)',
 	JWT_SECRET: process.env.JWT_SECRET,
-	CLIENT_ORIGIN: process.env.CLIENT_ORIGIN || 'http://jamclouds.now.sh/'
+	CLIENT_ORIGIN: process.env.CLIENT_ORIGIN || 'https://jamclouds.now.sh/'
 };
