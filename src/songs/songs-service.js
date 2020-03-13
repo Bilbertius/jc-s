f072a171b235da998f8c36bf70fe5b31b2f99976
Bilbertius <lauchlinmacneil@gmail.com>
2020-03-12 23:53:22 -0600

    Adds images to README.md

--- src/songs/songs-service.js
+++ src/songs/songs-service.js
@@ -1,9 +1,21 @@
  const xss = require('xss');
 
 const SongsService = {
+	/*
+	The following functions are tasked with providing the api with an interface to
+	utulize the database.
+	 */
+	
+	/*
+	Grabs all songs from the table called 'jams'
+	 */
 	getAllSongs(db) {
 		return db.select('*').from('jams')
 	},
+	
+	/*
+	Inserts a user created song into the table called 'jams'
+	 */
 	insertSong(db, newSong) {
 		return db
 			.insert(newSong)
@@ -13,6 +25,10 @@ const SongsService = {
 				return rows[0]
 			})
 	},
+	
+	/*
+	Finds a song based on the songs unique id
+	 */
 	getById(db, id) {
 		return db
 			.from('jams')
@@ -20,6 +36,10 @@ const SongsService = {
 			.where({ id })
 			.first()
 	},
+	
+	/*
+	Removes a song from the database based on its unique id
+	 */
 	deleteSong(db, id) {
 		return db('jams')
 			.where({ id })
@@ -47,7 +67,7 @@ const SongsService = {
 				date: song.show_date
 			}
 		}
-}
+};
 
 
 module.exports =  SongsService; 
